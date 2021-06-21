d72a2d2398f11293675f6de10aae8453bc29d961
John A. De Goes <john@degoes.net>
2021-06-21 10:54:54 -0400

    Update Discord link

--- src/main/scala/welcome.scala
+++ src/main/scala/welcome.scala
@@ -3,7 +3,7 @@ def assertTypesEqual[A, B >: A <: A]: Unit = ()
 object welcome:
   // Please join and say hello:
   //
-  // CHAT ROOM: https://discord.gg/dAbfksd
+  // CHAT ROOM: https://discord.gg/bc62QxMWqd
   //
   // Please git clone and build:
   //
