2f750481d6bf706e1c3160b0a2b3f5d2e3eed044
John A. De Goes <john@degoes.net>
2021-06-20 22:44:27 -0400

    add missing files

---
+++ src/main/scala/08-inline.scala
@@ -0,0 +1,217 @@
+import scala.language.implicitConversions
+
+/**
+ * INLINE
+ * 
+ * Scala 3 introduces a powerful new construct called `inline` to inline designated methods at their
+ * point of application. In addition to providing a tool to improve performance in hot spots that 
+ * could benefit from inlining, `inline` provides a limited form of compile-time evaluation of Scala 
+ * code, which is useful for metaprogramming.
+ */
+object inline_basics:
+  /**
+   * EXERCISE 1.
+   * 
+   * Guarantee the Scala compiler will inline the following constant by using the `inline` keyword.
+   * Then explicitly ascribe a type and note your findings.
+   */
+  val LoggingEnabled = false 
+
+  /**
+   * EXERCISE 2
+   * 
+   * Guarantee the Scala compiler will inline the following method anywhere it is applied, by using 
+   * the `inline` keyword.
+   */
+  def log(line: => String): Unit = 
+    if (LoggingEnabled) println(line)
+
+  /**
+   * EXERCISE 3
+   * 
+   * Simplify the following expression as the Scala compiler would simplify it.
+   */
+  val _ = log("This is a test!")
+
+object inline_recursion:
+  /**
+   * EXERCISE 1
+   * 
+   * Using recursion, implement the power function, which raises the specified number to the 
+   * specified integral power.
+   */
+  inline def pow(num: Float, exp: Int): Float = ???
+  /**
+   * EXERCISE 2
+   * 
+   * Compute the power of 10.5 raised to the power of 3.
+   */
+  def `10.5 ^ 3` = ???
+
+  /**
+   * EXERCISE 3
+   * 
+   * Try to compute the power of 10.5 raised to the power of 100 and note what happens.
+   */
+  def `10.5 ^ 100` = ???
+
+object inline_transparent:
+  final case class Natural(value: Int)
+  object Natural:
+     /**
+      * EXERCISE 1
+      * 
+      * Apply the `transparent` modifier to this use of `inline` so the specific subtype of the 
+      * return value will be reflected at the application site at compile-time.
+      */
+    inline def apply(v: Int): Option[Natural] = 
+      if (v >= 0) Some(new Natural(v)) else None
+
+    def fromInt(v: Int): Option[Natural] = apply(v)
+  end Natural
+  
+  /**
+   * EXERCISE 2
+   * 
+   * Define an implicit conversion from `Some[Natural]` to `Natural`.
+   */
+  given Conversion[Some[Natural], Natural] = ???
+
+  /**
+   * EXERCISE 3
+   * 
+   * Change the type of `zero` to `Natural`, and explain why this does or does not compile.
+   */
+  val zero: Option[Natural] = Natural(0)
+
+object inline_conditional:
+  import scala.compiletime.*
+
+  final case class Natural(value: Int)
+  object Natural:
+     /**
+      * EXERCISE 1
+      * 
+      * Apply the `inline` modifier to the conditional to guarantee the branch can be determined at 
+      * compile time.
+      */
+    transparent inline def apply(v: Int): Any = 
+      inline if (v >= 0) new Natural(v) else error("Not a natural number")
+
+    def fromInt(v: Int): Option[Natural] = 
+      if (v >= 0) Some(new Natural(v)) else None
+  end Natural
+
+  /**
+   * EXERCISE 2
+   * 
+   * Create a natural number from an integer literal.
+   */
+  def natural: Natural = ???
+
+  /**
+   * EXERCISE 3
+   * 
+   * Try to create a natural number from `fortyTwo`.
+   */
+  def naturalFortyTwo: Natural = ???
+  val fortyTwo: Int = 42
+
+object inline_match:
+  sealed trait Natural
+  case object Zero extends Natural
+  final case class Succ[N <: Natural](value: N) extends Natural
+
+  /**
+   * EXERCISE 1
+   * 
+   * Using an inline match and recursion, implement the following function, which converts a Natural
+   * number into an integer.
+   */
+  transparent inline def toInt(n: Natural): Int = ???
+
+  /**
+   * EXERCISE 2
+   * 
+   * Use the `toInt` function to convert `two` into an int. Ascribe it the most precise type you 
+   * can.
+   */
+  final val twoToInt = ???
+
+  def two: Succ[Succ[Zero.type]] = Succ(Succ(Zero))
+
+object compiletime:
+  import scala.compiletime.*
+  /**
+   * EXERCISE 1
+   * 
+   * Using the function `constValue`, implement the following `succ` function, which returns the 
+   * number after the number having the specified (singleton) type.
+   * 
+   * Hint: You will have to mark `succ` as inline to call this function.
+   */
+  def succ[N <: Int]: Int = ???
+
+  /**
+   * EXERCISE 2
+   * 
+   * Call the function `succ` on the type `3`. Make any changes to `succ` that are necessary to 
+   * type the result of `succ[3]` as `4`.
+   */
+  def four = ???
+
+  /**
+   * EXERCISE 3
+   * 
+   * Using the function `constValueOpt`, implement the following `ClassTag` function, which 
+   * should return a `Some(n)` if the specified type is a singleton integer, and `None` 
+   * otherwise.
+   */
+  inline def natural[A]: Option[Int] = ???
+
+  /**
+   * EXERCISE 4
+   * 
+   * Using inline match and the type-level function `S[N]`, which returns the successor type of the 
+   * singleton type `N`, implement the following function, which "counts" `N`.
+   */
+  transparent inline def count[N]: Int =
+    ??? // inline constValue[N] match
+
+  /**
+   * EXERCISE 5
+   * 
+   * Using `erasedValue` match against the runtime type of a value so you can return a default value
+   * of that type.
+   */
+  inline def defaultValue[A]: Option[A] = ???
+
+  /**
+   * EXERCISE 6
+   * 
+   * Using `error`, produce a compile-time failure if the provided number is less than 0.
+   */
+  inline def assertNatural(n: Int): Unit = ???
+
+object selective_summon:
+  import scala.compiletime.summonFrom
+
+  /**
+   * EXERCISE 1
+   * 
+   * Using `summonFrom`, create a function that makes a set for the provided type: if the type has 
+   * an ordering, then use `TreeSet`, otherwise, use `HashSet`.
+   */
+  inline def setFor[T]: Set[T] = ???
+
+object inline_params:
+  inline val LoggingLevel = 1
+
+  /**
+   * EXERCISE 1
+   * 
+   * Use inline parameters to ensure the following has no runtime overhead
+   * unless logging is enabled.
+   */
+  def log(level: Int, line: String): Unit = 
+    if (level >= LoggingLevel) println(line)
---
+++ src/main/scala/10-macros.scala
@@ -0,0 +1,88 @@
+import scala.quoted.*
+
+/**
+ * MACROS
+ * 
+ * Scala 3 introduces statically-typed, hygienic macros. 
+ */
+object macro_basics:
+  inline def assert(inline expr: Boolean): Unit =
+    ${assertImpl('expr)}
+
+  def assertImpl(expr: Expr[Boolean])(using Quotes): Expr[Any] = '{
+    val evaluate = $expr
+
+    if (!evaluate) throw new AssertionError(s"Failed assertion: ${${Expr(expr.show)}}")
+  }
+
+  // inline def identity1[A](inline expr: A): A = ${'{expr}}
+  
+  // inline def identity2[A](inline expr: Expr[A])(using Quotes): Expr[A] = '{${expr}}
+  
+  // inline def identityT1[A](a: A)(using Quotes): ${'[A]} = a
+
+  /**
+   * EXERCISE 1
+   * 
+   * Construct an `Expr[Int]` by using the `Expr.apply` constructor on an int literal.
+   */
+  def exprInt: Expr[Int] = ???
+
+  /**
+   * EXERCISE 2
+   * 
+   * Construct an `Expr[String]` by using the `Expr.apply` constructor on a string literal.
+   */
+  def exprString: Expr[String] = ???
+
+  /**
+   * EXERCISE 3
+   * 
+   * Turn this function into a macro by adding `inline` and delegating implementation to 
+   * the compile-time function `assertEqualsImpl`. You will have to use splice and quote.
+   * 
+   * Also inline the parameters so they will be fully expanded in the macro.
+   */
+  def assertEquals[A](expected: A, actual: A): Unit = ???
+
+  /**
+   * EXERCISE 4
+   * 
+   * Implement a compile-time function that generates the logic to check to see if the 
+   * actual value is equal to the expected value, and if not, prints out a useful message
+   * of the expressions. You will have to use quotation and splicing, as well as 
+   * `Expr#show`.
+   */
+  def assertEqualsImpl(expected: Expr[Any], actual: Expr[Any])(using Quotes): Expr[Unit] =
+    ???
+
+  /**
+   * EXERCISE 5
+   * 
+   * Turn the following function into a macro, which delegates to the compile-time 
+   * function `inspectTypeImpl`.
+   */
+  def inspectType[A]: TypeDetails = ???
+
+  def inspectTypeImpl[A](using quotes: Quotes, tpe: Type[A]): Expr[TypeDetails] =
+    import quotes.reflect.* 
+
+    /**
+     * EXERCISE 6
+     * 
+     * Capture some details about the representation of the type into the 
+     * `TypeDetails` structure. You may find documentation 
+     * [here](https://github.com/lampepfl/dotty/blob/master/library/src/scala/quoted/Quotes.scala).
+     */
+    def inspectTypeRepr(repr: TypeRepr): Expr[TypeDetails] = 
+      '{TypeDetails()}
+
+    inspectTypeRepr(TypeRepr.of[A])
+
+  final case class TypeDetails()
+
+
+  
+
+
+
